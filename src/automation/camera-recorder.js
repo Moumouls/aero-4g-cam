41a57d081bf8ff22419f1aa2bdc33385d5d4fe40
Antoine Cormouls <contact.antoine.cormouls@gmail.com>
2026-01-25 10:31:29 +0100

    18sec

--- src/automation/camera-recorder.js
+++ src/automation/camera-recorder.js
@@ -151,7 +151,7 @@ async function recordCamera() {
         });
 
         // wait 10 seconds before stopping the recording
-        await sleep(20000);
+        await sleep(18000);
 
         const videoBase64 = await driver.stopRecordingScreen();
 
